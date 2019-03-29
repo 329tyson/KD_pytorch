9f26bebb246d63393d463f95097d965e4c959062
Juwon-cvlab <gjw0917@postech.ac.kr>
2019-03-29 22:00:22 +0900

    clear coding

--- codes/main.py
+++ codes/main.py
@@ -34,6 +34,7 @@ if __name__ == '__main__':
     # - args.dataset = None
     # - args.classes = 0
     # - args.lr = 0.001
+    # - args.wd = 0.0005
     # - args.batch = 111
     # - args.epochs = 200
     # - args.resume = False
@@ -74,6 +75,22 @@ if __name__ == '__main__':
     else:
         args.classes = 196
 
+    low_img_need = args.fsr_enabled or args.kd_enabled or args.sr_enabled
+    try:
+        train_loader, eval_train_loader, eval_validation_loader, num_training, num_validation = generate_dataset(
+            args.dataset,
+            args.batch,
+            args.annotation_train,
+            args.annotation_val,
+            args.data,
+            args.low_ratio,
+            args.ten_batch_eval,
+            args.verbose,
+            low_img_need)
+    except ValueError:
+        print('inapproriate dataset, please put cub or stanford')
+        exit()
+
     if args.vgg_gap :
         vgg16 = models.vgg16(True)
         net = VGG_gap(vgg16, args.classes)
@@ -85,24 +102,58 @@ if __name__ == '__main__':
             net.parameters(),
             lr=args.lr,
             momentum=0.9,
-            weight_decay=0.0005)
+            weight_decay=args.wd)
+
+        if low_img_need:
+            teacher_net = VGG_gap(vgg16, args.classes)
+            teacher_net.load_state_dict(net.state_dict())
+            teacher_net.cuda()
 
     else:
-        net = AlexNet(0.5, args.classes, ['fc8'], save_layer=args.gram_features)
-        load_weight(net, args.pretrain_path)
+        if args.sr_enabled:
+            net = RACNN(0.5, args.classes, ['fc8'], alex_weights_path=args.pretrain_path,
+                        sr_weights_path=args.sr_pretrain_path)
 
-        optimizer= optim.SGD(
-            [{'params':net.conv1.parameters()},
-             {'params':net.conv2.parameters()},
-             {'params':net.conv3.parameters()},
-             {'params':net.conv4.parameters()},
-             {'params':net.conv5.parameters()},
-             {'params':net.fc6.parameters()},
-             {'params':net.fc7.parameters()},
-             {'params':net.fc8.parameters(), 'lr':args.lr * 10}],
-            lr=args.lr,
-            momentum = 0.9,
-            weight_decay = 0.0005)
+            optimizer = optim.SGD(
+                [{'params': net.classificationLayer.conv1.parameters()},
+                 {'params': net.classificationLayer.conv2.parameters()},
+                 {'params': net.classificationLayer.conv3.parameters()},
+                 {'params': net.classificationLayer.conv4.parameters()},
+                 {'params': net.classificationLayer.conv5.parameters()},
+                 {'params': net.classificationLayer.fc6.parameters()},
+                 {'params': net.classificationLayer.fc7.parameters()},
+                 {'params': net.classificationLayer.fc8.parameters(), 'lr': args.lr * 10}],
+                lr=args.lr,
+                momentum=0.9,
+                weight_decay=args.wd)
+        else:
+            net = AlexNet(0.5, args.classes, ['fc8'], save_layer=args.gram_features, residual_layer=args.adapter_features)
+            load_weight(net, args.pretrain_path)
+
+            optimizer= optim.SGD(
+                [{'params':net.conv1.parameters()},
+                 {'params':net.conv2.parameters()},
+                 {'params':net.conv3.parameters()},
+                 {'params':net.conv4.parameters()},
+                 {'params':net.conv5.parameters()},
+                 {'params':net.fc6.parameters()},
+                 {'params':net.fc7.parameters()},
+                 {'params':net.fc8.parameters(), 'lr':args.lr * 10},
+                 {'params':net.adapters.parameters()}],
+                lr=args.lr,
+                momentum=0.9,
+                weight_decay=args.wd)
+
+        if low_img_need:
+            # FIXME: if training_FSR, no need for teacher_net
+            if args.fsr_enabled:
+                exit()
+            teacher_net = AlexNet(0.5, args.classes, ['fc8'], args.gram_features)
+            ### for single gpu
+            load_weight(teacher_net, args.pretrain_path)
+            ### for multi gpu
+            # teacher_net.load_state_dict(net.state_dict())
+            teacher_net.cuda()
 
     net.cuda()
 
@@ -115,6 +166,7 @@ if __name__ == '__main__':
         print('Training Feature Super Resolution GAN model')
         print('\tLow resolution scaling = {} x {}'.format(args.low_ratio, args.low_ratio))
 
+        # FSR-GAN model
         generator = FSR_Generator()
         discriminator = FSR_Discriminator()
 
@@ -124,74 +176,34 @@ if __name__ == '__main__':
         generator.cuda()
         discriminator.cuda()
 
-        try:
-            train_loader, eval_train_loader, eval_validation_loader, num_training, num_validation = generate_dataset(
-                args.dataset,
-                args.batch,
-                args.annotation_train,
-                args.annotation_val,
-                args.data,
-                args.low_ratio,
-                args.ten_batch_eval,
-                args.verbose,
-                args.fsr_enabled)
-        except ValueError:
-            print('inapproriate dataset, please put cub or stanford')
-
+        # Logger
         logger = getlogger(args.log_dir + '/FSR-GAN_{}_LOW_{}'
                            .format(args.dataset, str(args.low_ratio)))
-        logger.debug(args.message)
         for arg in vars(args):
             logger.debug('{} - {}'.format(str(arg), str(getattr(args, arg))))
         logger.debug(
             '\nTraining FSR-GAN model, Low resolution of {}x{}'.format(str(args.low_ratio),
                                                                        str(args.low_ratio)))
         logger.debug('\t on ' + args.dataset.upper() + ' dataset, with hyper parameters above\n\n')
+
         # training_FSR(net, generator, discriminator, optimizer_G, optimizer_D, args.focal_loss_r,
                      # args.classes, args.lr, args.lr_decay, args.epochs, args.ten_batch_eval,
                      # train_loader, eval_train_loader, eval_validation_loader, num_training, num_validation,
                      # args.low_ratio, args.result, logger, args.vgg_gap, args.save)
-        teacher_net = AlexNet(0.5, args.classes, ['fc8'], args.gram_features)
-        load_weight(teacher_net, args.pretrain_path)
-        teacher_net.cuda()
 
         training_Disc(teacher_net, net, optimizer, discriminator, optimizer_D, args.w_clip,
                      args.lr, args.lr_decay, args.epochs, args.ten_batch_eval,
                      train_loader, eval_train_loader, eval_validation_loader, num_training, num_validation,
                      args.low_ratio, args.result, logger, args.vgg_gap, args.save)
 
-    elif args.kd_enabled is True:
+    elif args.kd_enabled:
         if args.low_ratio == 0:
             print('Invalid argument, choose low resolution (50 | 25)')
         else:
             print('\nTraining Knowledge Distillation model')
             print('\t on ',args.dataset,' with hyper parameters above')
             print('\tLow resolution scaling = {} x {}'.format(args.low_ratio, args.low_ratio))
-            if not args.vgg_gap:
-                teacher_net = AlexNet(0.5, args.classes, ['fc8'], args.gram_features)
-                # for single gpu
-                load_weight(teacher_net, args.pretrain_path)
-
-                ## for multi gpu
-                # teacher_net.load_state_dict(net.state_dict())
-                teacher_net.cuda()
-            else:
-                teacher_net = VGG_gap(vgg16, args.classes)
-                teacher_net.load_state_dict(net.state_dict())
-                teacher_net.cuda()
-            try:
-                train_loader, eval_train_loader, eval_validation_loader, num_training, num_validation = generate_dataset(
-                    args.dataset,
-                    args.batch,
-                    args.annotation_train,
-                    args.annotation_val,
-                    args.data,
-                    args.low_ratio,
-                    args.ten_batch_eval,
-                    args.verbose,
-                    args.kd_enabled)
-            except ValueError:
-                print('inapproriate dataset, please put cub or stanford')
+            print('\nTraining starts')
 
             # To execute folloing calculate_grad*, batch_size should be 1 and img_path should be contained in dataloader results
             # Use only for visualization
@@ -200,7 +212,6 @@ if __name__ == '__main__':
             # raise Stop
 
             if args.gram_enabled:
-                print('\nTraining starts')
                 if args.gram_features is not None:
                     logger = getlogger(args.log_dir + '/KD_DATASET_{}_LOW_{}_MSE_{}_WEIGHT_{}_RATIO_{}'
                                    .format(args.dataset, str(args.low_ratio), args.gram_features.replace(' ',''), str(args.style_weight), str(args.at_ratio)))
@@ -208,13 +219,13 @@ if __name__ == '__main__':
                     logger = getlogger(args.log_dir + '/KD_DATASET_{}_LOW_{}'
                                    .format(args.dataset, str(args.low_ratio)))
 
-                logger.debug(args.message)
                 for arg in vars(args):
                     logger.debug('{} - {}'.format(str(arg), str(getattr(args, arg))))
                 logger.debug(
                     '\nTraining model with KD & Gram, Low resolution of {}x{}'.format(str(args.low_ratio),
                                                                                               str(args.low_ratio)))
                 logger.debug('\t on ' + args.dataset.upper() + ' dataset, with hyper parameters above\n\n')
+
                 training_Gram_KD(
                     teacher_net,
                     net,
@@ -243,9 +254,7 @@ if __name__ == '__main__':
                     args.c_at,
                     args.s_at
                 )
-            # else for gram_enabled
             else:
-                print('\nTraining starts')
                 logger = getlogger(args.log_dir + '/KD_DATASET_{}_LOW_{}'
                                    .format(args.dataset, str(args.low_ratio)))
                 for arg in vars(args):
@@ -272,40 +281,21 @@ if __name__ == '__main__':
                     args.vgg_gap,
                     args.save
                     )
-    elif args.sr_enabled is True:
+    elif args.sr_enabled:
         if args.low_ratio == 0:
             print('Invalid argument, choose low resolution (50 | 25)')
         else:
             print('\nTraining Attention SR model')
             print('\t on ',args.dataset,' with hyper parameters above')
             print('\tLow resolution scaling = {} x {}'.format(args.low_ratio, args.low_ratio))
-            teacher_net = AlexNet(0.5, args.classes, ['fc8'], args.gram_features)
-            load_weight(teacher_net, args.pretrain_path)
-            net = RACNN(0.5, args.classes, ['fc8'], alex_weights_path = args.pretrain_path, sr_weights_path = args.sr_pretrain_path)
-            net.cuda()
-            teacher_net.cuda()
-            try:
-                train_loader, eval_train_loader, eval_validation_loader, num_training, num_validation = generate_dataset(
-                    args.dataset,
-                    args.batch,
-                    args.annotation_train,
-                    args.annotation_val,
-                    args.data,
-                    args.low_ratio,
-                    args.ten_batch_eval,
-                    args.verbose,
-                    args.sr_enabled)
-            except ValueError:
-                print('inapproriate dataset, please put cub or stanford')
             print('\nTraining starts')
+
             if args.gram_features is not None:
                 logger = getlogger(args.log_dir + '/SR_DATASET_{}_LOW_{}_MSE_{}_WEIGHT_{}_RATIO_{}'
                                    .format(args.dataset, str(args.low_ratio), args.gram_features.replace(' ',''), str(args.style_weight), str(args.at_ratio)))
             else:
                 logger = getlogger(args.log_dir + '/SR_DATASET_{}_LOW_{}'
                                    .format(args.dataset, str(args.low_ratio)))
-
-            # logger.debug(args.message)
             for arg in vars(args):
                 logger.debug('{} - {}'.format(str(arg), str(getattr(args, arg))))
             logger.debug(
@@ -313,27 +303,6 @@ if __name__ == '__main__':
                                                                                               str(args.low_ratio)))
             logger.debug('\t on ' + args.dataset.upper() + ' dataset, with hyper parameters above\n\n')
 
-            # optimizer = optim.SGD(
-            #          [{'params':net.srLayer.parameters(), 'lr': 0.0},
-            #          {'params':net.get_all_params_except_last_fc(), 'lr': 0.1 * args.lr},
-            #          {'params':net.classificationLayer.fc8.weight, 'lr': 1.0 * args.lr,
-            #           'weight_decay': 1.0 * 0.0005},
-            #          {'params':net.classificationLayer.fc8.bias, 'lr': 2.0 * args.lr,
-            #           'weight_decay': 0.0}],
-            #          momentum=0.95, weight_decay=0.0005)
-
-            optimizer = optim.SGD(
-                [{'params': net.classificationLayer.conv1.parameters()},
-                 {'params': net.classificationLayer.conv2.parameters()},
-                 {'params': net.classificationLayer.conv3.parameters()},
-                 {'params': net.classificationLayer.conv4.parameters()},
-                 {'params': net.classificationLayer.conv5.parameters()},
-                 {'params': net.classificationLayer.fc6.parameters()},
-                 {'params': net.classificationLayer.fc7.parameters()},
-                 {'params': net.classificationLayer.fc8.parameters(), 'lr': args.lr * 10}],
-                lr=args.lr,
-                momentum=0.9,
-                weight_decay=0.0005)
             training_attention_SR(
                 teacher_net,
                 net,
@@ -365,27 +334,14 @@ if __name__ == '__main__':
         if args.low_ratio == 0:
             print('\nTraining High Resolution images')
             print('\t on ',args.dataset.upper(),' dataset, with hyper parameters above')
-
-            try:
-                train_loader, eval_train_loader, eval_validation_loader, num_training, num_validation = generate_dataset(
-                    args.dataset,
-                    args.batch,
-                    args.annotation_train,
-                    args.annotation_val,
-                    args.data,
-                    args.low_ratio,
-                    args.ten_batch_eval,
-                    args.verbose)
-            except ValueError:
-                print('inapproriate dataset, please put cub or stanford')
-                exit
-
             print('\nTraining starts')
+
             logger = getlogger(args.log_dir + '/DATASET_{}_HIGH_RES'.format(args.dataset))
             for arg in vars(args):
                 logger.debug('{} - {}'.format(str(arg), str(getattr(args, arg))))
             logger.debug('\nTraining High Resolution images')
             logger.debug('\t on '+args.dataset.upper()+' dataset, with hyper parameters above\n\n')
+
             training(net, optimizer,
                      args.lr, args.lr_decay, args.epochs, args.ten_batch_eval,
                      train_loader, eval_train_loader, eval_validation_loader, num_training, num_validation,
@@ -398,26 +354,14 @@ if __name__ == '__main__':
             print('\nTraining Low Resolution images')
             print('\t on ',args.dataset,' with hyper parameters above')
             print('\tLow resolution scaling = {} x {}'.format(args.low_ratio, args.low_ratio))
-
-            try:
-                train_loader, eval_train_loader, eval_validation_loader, num_training, num_validation = generate_dataset(
-                    args.dataset,
-                    args.batch,
-                    args.annotation_train,
-                    args.annotation_val,
-                    args.data,
-                    args.low_ratio,
-                    args.ten_batch_eval,
-                    args.verbose)
-            except ValueError:
-                print('inapproriate dataset, please put type or stanford')
-
             print('\nTraining starts')
+
             logger = getlogger(args.log_dir + '/DATASET_{}_LOW_{}'.format(args.dataset, str(args.low_ratio)))
             for arg in vars(args):
                 logger.debug('{} - {}'.format(str(arg), str(getattr(args, arg))))
             logger.debug('\nTraining Low Resolution images, Low resolution of {}x{}'.format(str(args.low_ratio), str(args.low_ratio)))
             logger.debug('\t on '+args.dataset.upper()+' dataset, with hyper parameters above\n\n')
+
             training(net, optimizer,
                      args.lr, args.lr_decay, args.epochs, args.ten_batch_eval,
                      train_loader, eval_train_loader, eval_validation_loader, num_training, num_validation,
